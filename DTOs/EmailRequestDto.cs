243b75de3cb18a4f3f3e1714997f75ca5add57cc
Alfred Mukonda <amukonda@cbz.co.zw>
2025-03-03 09:21:57 +0200

    Add Model Classes for Email Attachment

--- DTOs/EmailRequestDto.cs
+++ DTOs/EmailRequestDto.cs
@@ -1,17 +1,77 @@
 ﻿using Email.Models;
+using System.Xml.Serialization;
 
 namespace Email.DTOs
 {
+    //public class EmailRequestDto
+    [XmlRoot("request")]
     public class EmailRequestDto
     {
+        [XmlElement("header")]
+        public Header Header { get; set; }
+
+        [XmlElement("body")]
+        public Body Body { get; set; }
+    }
+    public class Header
+    {
+        [XmlElement("systemName")]
+        public string SystemName { get; set; }
+
+        [XmlElement("tranType")]
+        public string TranType { get; set; }
+
+        [XmlElement("requestXML")]
+        public string RequestXML { get; set; }
+
+        [XmlElement("referenceNumber")]
+        public string ReferenceNumber { get; set; }
+    }
+
+    public class Body
+    {
+        [XmlElement("from")]
         public string From { get; set; }
+
+        [XmlArray("to")]
+        [XmlArrayItem("recipient")]
         public List<string> To { get; set; }
+
+        [XmlArray("cc")]
+        [XmlArrayItem("cc_recipient")]
         public List<string> Cc { get; set; }
+
+        [XmlArray("bcc")]
+        [XmlArrayItem("bcc_recipient")]
         public List<string> Bcc { get; set; }
+
+        [XmlElement("subject")]
         public string Subject { get; set; }
-        public string Body { get; set; }
+
+        [XmlElement("body")]
+        public string EmailBody { get; set; }
+
+        [XmlArray("attachments")]
+        [XmlArrayItem("attachment")]
         public List<Attachment> Attachments { get; set; }
+
+        [XmlElement("replyTo")]
         public string ReplyTo { get; set; }
+
+        [XmlElement("priority")]
         public string Priority { get; set; }
     }
+
+    public class Attachment
+    {
+        [XmlElement("filename")]
+        public string Filename { get; set; }
+
+        [XmlElement("contentType")]
+        public string ContentType { get; set; }
+
+        [XmlElement("content")]
+        public string Content { get; set; }
+    }
+
 }
